9ead53ad3a5495c8df142ab7461f2669e1115733
Yerassyl <tleugazy98@gmail.com>
2021-07-28 22:33:09 +0600

    commits

--- login_type.go
+++ login_type.go
@@ -6,3 +6,22 @@ var (
 	Email       LoginType = "email"
 	PhoneNumber LoginType = "phone_number"
 )
+
+var (
+	loginTypeToString = map[LoginType]string{
+		Email:       "email",
+		PhoneNumber: "phone_number",
+	}
+	stringToLoginType = map[string]LoginType{
+		"email":        Email,
+		"phone_number": PhoneNumber,
+	}
+)
+
+func (l LoginType) ToString() string {
+	return loginTypeToString[l]
+}
+
+func ToLoginType(s string) LoginType {
+	return stringToLoginType[s]
+}
