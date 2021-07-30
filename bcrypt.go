6be1471e1b1f20774ee17ba9c2c63e89a289511c
Yerassyl <tleugazy98@gmail.com>
2021-07-30 06:47:56 +0600

    commits

--- bcrypt.go
+++ bcrypt.go
@@ -3,6 +3,7 @@ package setdata_common
 import (
 	"golang.org/x/crypto/bcrypt"
 )
+
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
@@ -12,4 +13,3 @@ func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
 }
-
