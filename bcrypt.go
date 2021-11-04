ab6a82248106722647a7a8f7c5f51f3e2da911ea
Yerassyl Tleugazy <tleugazy98@gmail.com>
2021-11-04 19:01:43 +0600

    commits

--- bcrypt.go
+++ bcrypt.go
@@ -5,7 +5,7 @@ import (
 )
 
 func HashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 4)
 	return string(bytes), err
 }
 
